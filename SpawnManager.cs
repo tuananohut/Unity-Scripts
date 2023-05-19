c453c53dfc65ac197994fc7dd3b1fc37e2d5e92c
Tuana <57767763+tuananohut@users.noreply.github.com>
2023-05-19 14:57:23 +0300

    Create SpawnManager.cs
    Kontrollü spawn.

---
+++ SpawnManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefab;
+    private float spawnRangeX = 15;
+    private float spawnPosZ = 20;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefab.Length);
+            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
+        }
+
+
+    }
+}
